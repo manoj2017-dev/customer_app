7ea32c894b30fca6ce485fad3acb8e778b1b5461
manoj2017-dev <manoj.nkumar2017@gmail.com>
2025-04-17 15:07:37 +0530

    updating the website
    working on below points
    2.Ten books in the website to read(if they want to read this book they need to signup or if the customer is already signed up then need to login)
    3.contribution if anyone needs to add books the present customer can add the books

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,28 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import Config
 
+# Initialize extensions
 mongo = PyMongo()
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # Initialize extensions
     mongo.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
 
-    from app.routes import main
-    app.register_blueprint(main)
+    # Import models so they are registered with SQLAlchemy
+    from .models import Book, User  # Add your models here
+
+    # Import and register blueprints
+    from .routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
